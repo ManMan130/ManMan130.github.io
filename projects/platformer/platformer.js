bb7a50d7a6d636d451d1e495ff031783ada57183
ManMan130 <leroy.haynes@sciacademy.org>
2024-09-06 16:41:14 +0000

    message

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,8 +38,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+    createPlatform(100, 700, 200, 20);
+    createPlatform(500, 300, 200, 20);
 
     
     // TODO 2
